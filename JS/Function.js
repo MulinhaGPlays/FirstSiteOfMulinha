0d9e83f59cc41f05bb658fe8b72e6e1634683f35
MulinhaGPlays <filaucraft.eu@gmail.com>
2021-11-29 11:49:33 -0300

    FirstSiteOfMulinha

--- JS/Function.js
+++ JS/Function.js
@@ -1,4 +1,4 @@
 menu.onclick = function(){
-    menu.classList.toggle('active')
-    bg_menu.classList.toggle('active')
+    menu.classList.toggle('active');
+    bg_menu.classList.toggle('active');
 }

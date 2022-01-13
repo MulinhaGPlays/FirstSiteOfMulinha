f44ea5d5cf01ddf548d9afbf6a15ab61c056a4f0
MulinhaGPlays <filaucraft.eu@gmail.com>
2022-01-13 15:59:20 -0300

    FirstSiteOfMulinha

---
+++ JS/Comand.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 3000; i++) {
+    const span = document.createElement('span');
+    
+    bgl.appendChild(span);
+}
--- JS/Variables/GetElement_var.js
+++ JS/Variables/GetElement_var.js
@@ -1,2 +1,3 @@
 let menu = document.querySelector('.menu')
 let bg_menu = document.querySelector('.bg_menu')
+let bgl = document.querySelector('section')
